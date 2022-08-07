f8371f8f0a5055e7d482e8ca08b32b63a4e464d7
Emilien Leroy <emilien1005@hotmail.fr>
2022-08-07 19:31:01 +0200

    ✨ Create project usin Ts.ED cli

---
+++ src/config/mongoose/default.config.ts
@@ -0,0 +1,5 @@
+export default {
+  id: "default",
+  url: process.env.DEFAULT_URL || "mongodb://localhost:27017/default",
+  connectionOptions: { }
+};
---
+++ src/config/mongoose/index.ts
@@ -0,0 +1,6 @@
+// @tsed/cli do not edit
+import defaultConfig from "./default.config";
+
+export default [
+  defaultConfig
+];
