3069b00d67efdfebd0bbc770d2a552a08e73fa8f
Emilien Leroy <emilien1005@hotmail.fr>
2022-08-11 23:41:33 +0200

    🚧 Add list score on home view

--- src/home/HomeController.ts
+++ src/home/HomeController.ts
@@ -1,12 +1,18 @@
-import { Controller } from "@tsed/di";
+import { Controller, Inject } from "@tsed/di";
 import { Get } from "@tsed/schema";
 import {View} from "@tsed/platform-views";
+import { ScoreService } from "../scores";
 
 @Controller('/')
 export class HomeController {
+  @Inject(ScoreService)
+  private scoreService: ScoreService;
+
   @Get("/")
   @View("index.ejs")
-  public getHomeView() {
-    return {};
+  public async getHomeView() {
+    const scores = await this.scoreService.getScores();
+
+    return { scores };
   }
 }
