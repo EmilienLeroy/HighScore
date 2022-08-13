7eaa6165bbd2b783b7c93a2039878abd7ba2653e
Emilien Leroy <emilien1005@hotmail.fr>
2022-08-13 12:08:43 +0200

    🔨 Add release script

---
+++ scripts/version.js
@@ -0,0 +1,12 @@
+const { execSync } = require('child_process');
+const version = process.argv.at(2); 
+
+if (!version) {
+  throw new Error('Missing version !');
+}
+
+execSync(`chan release ${version}`);
+execSync(`git add .`);
+execSync(`npm version ${version} --force`);
+execSync('git push');
+execSync('git push --tags');
