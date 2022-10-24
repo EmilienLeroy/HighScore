07360201d6f85beae930fafd98267d65a71bc222
Emilien Leroy <emilien1005@hotmail.fr>
2022-10-24 07:12:59 +0200

    ✨ Add app version into metrics

--- src/config/metrics/index.ts
+++ src/config/metrics/index.ts
@@ -1,10 +1,22 @@
 import { Request, Response } from '@tsed/common';
-import { collectDefaultMetrics, Registry } from 'prom-client';
+import { collectDefaultMetrics, Gauge, Registry } from 'prom-client';
+import { version } from '../../../package.json';
 
 const register = new Registry();
 
 collectDefaultMetrics({ 
   register,
+});
+
+new Gauge({
+  name: 'nodejs_app_version_info',
+  help: 'Application version info.',
+  labelNames: ['version'],
+  registers: [register],
+  aggregator: 'first',
+  collect() {
+      this.labels(version).set(1);
+  },
 });
 
 const useMetrics = async (req: Request, res: Response) => {
