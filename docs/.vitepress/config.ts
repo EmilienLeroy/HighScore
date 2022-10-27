7cc674d8542a433bd4342f697d72736b3c7db23c
Emilien Leroy <emilien1005@hotmail.fr>
2022-10-27 07:13:40 +0200

    💄 Update docs themes

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,10 +4,29 @@ export default defineConfig({
 	title: 'HighScore',
   description: 'Open Source leaderboard.',
 	themeConfig: {
+    logo: '/logo.png',
+
 		socialLinks: [
       { icon: 'github', link: 'https://github.com/EmilienLeroy/HighScore' }
     ],
-		footer: {
+
+    nav: [
+      { text: 'Guide', link: '/guide/' },
+    ],
+
+    sidebar: {
+      '/guide/': [
+        {
+          text: 'Guide',
+          items: [
+            { text: 'Introduction', link: '/guide/' },
+            { text: 'Getting Started', link: '/guide/getting-started' },
+          ]
+        }
+      ]
+    },
+
+    footer: {
       message: 'Released under the MIT License.',
       copyright: 'Copyright © 2022-present Emilien Leroy'
     },
