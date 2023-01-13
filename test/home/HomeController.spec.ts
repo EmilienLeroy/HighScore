4d38e371ba4fc091d7842fd5d185419e94fbaf62
Emilien Leroy <emilien1005@hotmail.fr>
2023-01-13 07:26:45 +0100

    ✅ Add tests for /download and /privacy

--- test/home/HomeController.spec.ts
+++ test/home/HomeController.spec.ts
@@ -3,6 +3,7 @@ import { MongooseModel } from '@tsed/mongoose';
 import { TestMongooseContext } from '@tsed/testing-mongoose';
 import mongoose from 'mongoose';
 import SuperTest from 'supertest';
+import { envs } from '../../src/config/envs';
 import { Score } from '../../src/scores/Score';
 import { Server } from '../../src/Server';
 
@@ -122,6 +123,89 @@ describe('HomeController', () => {
       expect(text).toMatch(/Player 4/);
       expect(text).toMatch(/1500/);
       expect(text).toMatch(/hard/);
+    });
+  });
+
+  describe('GET /download', () => {
+    beforeAll(() => {
+      envs.HIGHSCORE_WINDOWS_DOWNLOAD_URL = '/windows';
+      envs.HIGHSCORE_LINUX_DOWNLOAD_URL = '/linux';
+      envs.HIGHSCORE_MACOS_DOWNLOAD_URL = '/macos';
+      envs.HIGHSCORE_ANDROID_DOWNLOAD_URL = '/android';
+      envs.HIGHSCORE_IOS_DOWNLOAD_URL = '/ios';
+    });
+
+    it('should redirect to the windows link', async () => {
+      const { status, text } = await request
+        .get('/download')
+        .set(
+          'User-Agent',
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
+        );
+
+      expect(status).toEqual(302);
+      expect(text).toEqual('Found. Redirecting to /windows');
+    });
+
+    it('should redirect to the linux link', async () => {
+      const { status, text } = await request
+        .get('/download')
+        .set('User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1');
+
+      expect(status).toEqual(302);
+      expect(text).toEqual('Found. Redirecting to /linux');
+    });
+
+    it('should redirect to the macos link', async () => {
+      const { status, text } = await request
+        .get('/download')
+        .set(
+          'User-Agent',
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9',
+        );
+
+      expect(status).toEqual(302);
+      expect(text).toEqual('Found. Redirecting to /macos');
+    });
+
+    it('should redirect to the android link', async () => {
+      const { status, text } = await request
+        .get('/download')
+        .set(
+          'User-Agent',
+          'Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36',
+        );
+
+      expect(status).toEqual(302);
+      expect(text).toEqual('Found. Redirecting to /android');
+    });
+
+    it('should redirect to the ios link', async () => {
+      const { status, text } = await request
+        .get('/download')
+        .set(
+          'User-Agent',
+          'Mozilla/5.0 (iPhone13,2; U; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/15E148 Safari/602.1',
+        );
+
+      expect(status).toEqual(302);
+      expect(text).toEqual('Found. Redirecting to /ios');
+    });
+  });
+
+  describe('GET /privacy', () => {
+    it('should get the privacy view with some custom information', async () => {
+      process.env.HIGHSCORE_PRIVACY_EMAIL = 'test@test.fr';
+      process.env.HIGHSCORE_PRIVACY_WEBSITE = 'privacy.test.fr';
+      process.env.HIGHSCORE_PRIVACY_COUNTRY = 'france';
+
+      const { status, text } = await request.get('/privacy').set('useragent', '');
+
+      expect(status).toEqual(200);
+      expect(text).toMatch(/Privacy/);
+      expect(text).toMatch(/test@test.fr/);
+      expect(text).toMatch(/privacy.test.fr/);
+      expect(text).toMatch(/france/);
     });
   });
 });
